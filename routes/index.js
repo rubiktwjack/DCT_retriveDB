f45b6fd25b6a11f5824bd133cb0435722c2adf48
ChiChan <rubiktwjack@gmail.com>
2018-06-21 01:28:09 +0800

    update 0621/0130

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,53 @@
 var express = require("express");
 var router = express.Router();
 var firebase = require("firebase");
-var fs = require('fs');
+var fs = require("fs");
 
 //set firebase
 var config = {
-    apiKey: "AIzaSyATOeCpLhJOwj2vuxNxdxpks_Y6j-80T2Y",
-    authDomain: "avascript-final-miband2.firebaseapp.com",
-    databaseURL: "https://avascript-final-miband2.firebaseio.com",
-    projectId: "avascript-final-miband2",
-    storageBucket: "avascript-final-miband2.appspot.com",
-    messagingSenderId: "60786103386"
+  apiKey: "AIzaSyATOeCpLhJOwj2vuxNxdxpks_Y6j-80T2Y",
+  authDomain: "avascript-final-miband2.firebaseapp.com",
+  databaseURL: "https://avascript-final-miband2.firebaseio.com",
+  projectId: "avascript-final-miband2",
+  storageBucket: "avascript-final-miband2.appspot.com",
+  messagingSenderId: "60786103386"
 };
 firebase.initializeApp(config);
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-    res.render("index", {title: "Express"});
-    // retrieve dtabase
-    var rate;
-    var database = firebase.database();
-    retriveDB();
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+  // retrieve dtabase
+  var rate, num;
+  var database = firebase.database();
+  retriveDB();
 
-    function retriveDB() {
-        database.ref("elderly/1/").on("value", function (snapshot) {
-            console.log(snapshot.val());
-            //console.log(snapshot.val().heartRate);
-            rate = snapshot.val().heartRate;
-            fs.writeFile('heartrate.txt', rate, function (err) {
-                if (err)
-                    console.log(err);
-                else
-                    console.log('Write operation complete.');
+  function retriveDB() {
+    database.ref("elderly/").on("value", function(snapshot) {
+      num = snapshot.numChildren();
+      for (var i = 1; i <= num; i++) {
+        database.ref("elderly/" + i + "/").on("value", function(snapshot) {
+          console.log(snapshot.val());
+          rate = snapshot.val().heartRate;
+          var dataString = rate + "\r\n";
+          //console.log(dataString);
+          if (i == 1) {
+            fs.writeFile("heartrate.txt", dataString, function(err) {
+              if (err) console.log(err);
+              else console.log("Write operation complete.");
             });
+          } else {
+            fs.appendFile("heartrate.txt", dataString, function(err) {
+              if (err) console.log(err);
+              else console.log("Append operation complete.");
+            });
+          }
         });
-    }
+      }
+    });
+  }
 
-    setTimeout('retriveDB()', 1000);
-    //console.log("test");
-    //console.log(rate);
+  setTimeout("retriveDB()", 1000);
 });
 
 module.exports = router;
